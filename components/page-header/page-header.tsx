cbe391ace6193428b1fe2be4d139759633c52fc4
Michal Drabik <micdra7@gmail.com>
2022-01-12 09:40:01 +0100

    feat: update color of page header in light mode

--- components/page-header/page-header.tsx
+++ components/page-header/page-header.tsx
@@ -1,12 +1,16 @@
-import { Heading } from '@chakra-ui/react';
+import { Heading, useColorModeValue } from '@chakra-ui/react';
 
 type TPageHeaderProps = {
   text: string;
   color?: string;
 };
 
-export const PageHeader = ({ text, color }: TPageHeaderProps): JSX.Element => (
-  <Heading size="2xl" color={color ?? 'initial'} w="100%">
-    {text}
-  </Heading>
-);
+export const PageHeader = ({ text, color }: TPageHeaderProps): JSX.Element => {
+  const filter = useColorModeValue('brightness(90%)', undefined);
+
+  return (
+    <Heading size="2xl" color={color ?? 'initial'} w="100%" filter={filter}>
+      {text}
+    </Heading>
+  );
+};
